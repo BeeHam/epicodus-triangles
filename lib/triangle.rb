b75093d6f08bffd6e7a67d0255f3e7eb715ca53f
Clinton Herrick & Brigham Thompson <student@epicodus.com>
2015-08-10 12:01:08 -0700

    add spec tests and methods for equilateral and isosceles

---
+++ lib/triangle.rb
@@ -0,0 +1,24 @@
+class Triangle
+ define_method(:initialize) do |side1, side2, side3|
+   @side1 = side1
+   @side2 = side2
+   @side3 = side3
+ end
+
+ define_method(:equilateral?) do
+   if @side1.eql?(@side2).&(@side1.eql?(@side3))
+     true
+   end
+ end
+
+
+ define_method(:isosceles?) do
+   if @side1.eql?(@side2).|(@side1.eql?(@side3))
+     true
+   elsif @side2.eql?(@side3)
+     true
+   else
+     false
+   end
+ end
+end
---
+++ spec/triangle_spec.rb
@@ -0,0 +1,25 @@
+require ('rspec')
+require ('triangle')
+
+describe (Triangle) do
+  describe ("#equilateral?") do
+    it("returns true if all sides aren't equal") do
+      test_triangle1 = Triangle.new(15, 15, 15)
+      expect(test_triangle1.equilateral?()).to(eq(true))
+    end
+  end
+
+  describe('#isosceles') do
+    it("returns true if any two sides are equal") do
+      test_triangle2 = Triangle.new(15, 15, 30)
+      expect(test_triangle2.isosceles?()).to(eq(true))
+    end
+  end
+
+  describe('#scalene') do
+    it("returns true if any two sides are equal") do
+      test_triangle3 = Triangle.new(15, 15, 30)
+      expect(test_triangle3.scalene?()).to(eq(true))
+    end
+  end
+end
